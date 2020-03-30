d2bc45cdc8b9ec764283a577d19446748ff325f2
Esterlyn Diaz <diazesterlin62@gmail.com>
2020-03-29 22:40:37 -0400

    Agrego ejercicio a codigo js

---
+++ ejercicios nuevos/19.js
@@ -0,0 +1,13 @@
+for (let a = 1; a < 20; a++) {
+    for (let b = a; b < 20; b++) {
+        for (let c = b; c < 20; c++) {
+           if (a**2+b**2==c**2) {
+               console.log(a,b,c);
+               
+           }
+            
+        }
+        
+    }
+    
+}
